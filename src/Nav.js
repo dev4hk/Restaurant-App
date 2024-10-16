f3ed5dc990800e52252d3dcd4c8d552b91e08923
Hoon <hoon0661@gmail.com>
2024-10-16 17:35:49 -0400

    added html for nav

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { ReactComponent as Logo } from "./images/Logo.svg";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Logo />
+        </li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Menu</a>
+        </li>
+        <li>
+          <a href="#">Reservations</a>
+        </li>
+        <li>
+          <a href="#">Order online</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
