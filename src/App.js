348ab4ed50f8dec64c9c6b5e4d18655e8d558ff5
Hoon <hoon0661@gmail.com>
2024-10-16 16:46:35 -0400

    Added html structure template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+import Nav from "./Nav";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
