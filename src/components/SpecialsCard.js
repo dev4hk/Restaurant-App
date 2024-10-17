2b6c700c23e6a746b8014acc181127090d1d5f6c
Hoon <hoon0661@gmail.com>
2024-10-17 19:13:34 -0400

    changed class name for specials card

--- src/components/SpecialsCard.js
+++ src/components/SpecialsCard.js
@@ -4,19 +4,19 @@ import GreekSalad from "../assets/images/greek salad.jpg";
 
 const SpecialsCard = () => {
   return (
-    <div className="card">
-      <img className="card-image" src={GreekSalad} alt="card-image" />
-      <div className="card-details">
-        <div className="card-title-wrapper">
+    <div className="specials-card">
+      <img className="specials-card-image" src={GreekSalad} alt="card-image" />
+      <div className="specials-card-details">
+        <div className="specials-card-title-wrapper">
           <h3 className="markazi-text card-title">Greek Salad</h3>
-          <span className="card-price">$ 12.99</span>
+          <span className="specials-card-price">$ 12.99</span>
         </div>
-        <p className="card-description">
+        <p className="specials-card-description">
           The famous greek salad of crispy lettuce, peppers, olives and out
           Chicago style feta cheese, garnished with crunchy garlic and rosemary
           croutons.
         </p>
-        <p className="card-delivery-text">Order a delivery</p>
+        <p className="specials-card-delivery-text">Order a delivery</p>
       </div>
     </div>
   );
