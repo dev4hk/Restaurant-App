a9503da0259fe0dc5359d582ce96d10142e8b4a6
Hoon <hoon0661@gmail.com>
2024-10-18 09:19:09 -0400

    added testimonials section in home

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,7 +1,20 @@
 import React from "react";
+import TestimonialsCard from "./TestimonialsCard";
 
 const Testimonials = () => {
-  return <div className="testimonials"></div>;
+  return (
+    <div className="testimonials">
+      <h2 className="markazi-text testimonials-title">Testimonials</h2>
+      <div className="testimonials-cards">
+        <div></div>
+        <TestimonialsCard />
+        <TestimonialsCard />
+        <TestimonialsCard />
+        <TestimonialsCard />
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
---
+++ src/components/TestimonialsCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TestimonialsCard = () => {
+  return (
+    <div className="testimonials-card">
+      <div className="testimonials-card-rate">5/5</div>
+      <div className="testimonials-card-profile">
+        <div className="testimonials-card-profile-image"></div>
+        <div className="testimonials-card-profile-name">Common Name</div>
+      </div>
+      <div className="testimonials-card-comment">
+        Thank you for great food and service. I enjoyed so much.
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialsCard;
