8b72c03361d679fddf6d9815db68377fdf2b0015
Hoon <hoon0661@gmail.com>
2024-10-21 09:29:11 -0400

    changed page name of reservations to bookingpage

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Nav from "./components/Nav";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Menu from "./routes/Menu";
-import Reservations from "./routes/Reservations";
 import OnlineOrder from "./routes/OnlineOrder";
 import Login from "./routes/Login";
 import Footer from "./components/Footer";
+import BookingPage from "./routes/BookingPage";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/menu" element={<Menu />}></Route>
-        <Route path="/reservations" element={<Reservations />}></Route>
+        <Route path="/reservations" element={<BookingPage />}></Route>
         <Route path="/order-online" element={<OnlineOrder />}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
